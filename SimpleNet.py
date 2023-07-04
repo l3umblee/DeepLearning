0be1f1075846fc4e276dcb1df0a91cb0e17f94bb
l3umblee <99014066+l3umblee@users.noreply.github.com>
2023-07-04 17:06:07 +0900

    Modify DeepLearningLB, Add Some Example Code (0704)

--- SimpleNet.py
+++ SimpleNet.py
@@ -13,11 +13,11 @@ self.W -> 가중치 매개변수
 preidct(self, x) : 가중치 매개변수와 입력 데이터를 내적 -> 단순한 1층 신경망
 loss(self, x, t) : z는 내적의 결과값, y는 z를 softmax에 통과시킨 예측값 -> loss는 y와 t를 교차 엔트로피 오차 구한 것
 
+
 '''
 class simpleNet:
     def __init__(self):
-        self.W = np.array([[0.47355232, 0.9977393, 0.84668094], 
-                           [0.8555741, 0.03563661, 0.69422093]]) #->가우시안 표준 정규 분포에서 난수 matrix array 생성
+        self.W = np.random.randn(2, 3) #->가우시안 표준 정규 분포에서 난수 matrix array 생성
     
     def predict(self, x):
         return np.dot(x, self.W)
@@ -37,6 +37,15 @@ p = net.predict(x)
 print(p)
 print(np.argmax(p))
 
-t = np.array([0, 0, 1])
-l = net.loss(x, t)
-print(l)
+t = np.array([0, 0, 0])
+for idx in range(0, len(t)):
+    if idx == np.argmax(p):
+        t[idx] = 1
+
+print(t)
+print(net.loss(x, t))
+
+f = lambda w: net.loss(x, t)
+
+dW = DL.numerical_gradient(f, net.W)
+print(dW)
