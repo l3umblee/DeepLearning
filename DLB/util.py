0cb221818ac3aa5fbbd80d8093edb66fa0bb255d
l3umblee <99014066+l3umblee@users.noreply.github.com>
2023-07-22 15:26:38 +0900

    Implement im2col(30%), Study tensor array

--- DLB/util.py
+++ DLB/util.py
@@ -31,3 +31,33 @@ def shuffle_dataset(x, t):
     t = t[permutation]
 
     return x, t
+
+#im2col : image to column이라는 뜻으로, 입력 데이터를 필터링하기 좋게 펼치는 함수, 쉽게 말해 일렬로 나열하는 함수
+'''
+사실 im2col을 사용해 합성곱 계층을 구현하면 메모리를 더 많이 소비하는 단점이 있지만
+컴퓨터는 큰 행렬을 묶어서 계산하는데 탁월하기 때문에 큰 행렬의 곱셈을 빠르게 계산할 수 있다.
+
+-> 행렬 계산은 선형 대수 라이브러리를 활용해 효율을 높일 수 있다.
+
+im2col의 결과는 2차원 행렬이다.
+------------------------------------------
+input_data - (데이터 수, 채널 수, 높이, 너비)의 4차원 배열로 이루어진 입력 데이터 (N, C, H, W)
+filter_h - 필터의 높이
+filter_w - 필터의 너비
+stride - 스트라이드
+pad - 패딩 (두께)
+'''
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    N = input_data.shape[0]  #데이터의 개수인 N이 들어감
+    C = input_data.shape[1]  #채널 수인 C가 들어감
+    H = input_data.shape[2]  #데이터의 높이인 H가 들어감
+    W = input_data.shape[3]  #데이터의 너비인 W가 들어감
+
+    #이 두 변수는 출력 크기의 높이와 너비를 나타내기도 하지만, 필터에 의해 입력 데이터를 출력하는 동작을 몇번할지도 결정할 수 있다.
+    oh = (H + 2*pad - filter_h) / stride + 1 #출력 크기의 높이
+    ow = (W + 2*pad - filter_w) / stride + 1 #출력 크기의 너비
+    
+    out = np.zero(N, (oh*ow)*(filter_h*filter_w))
+
+    for idx in range(N):
+        pass
--- pytest.py
+++ pytest.py
@@ -1,1 +1,9 @@
-print("No pytest")
+import numpy as np
+
+x = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+x1 = np.array([[[11, 12, 13], [14, 15, 16], [17, 18, 19]], [[11, 12, 13], [14, 15, 16], [17, 18, 19]]])
+x_final = np.array([x, x1])
+print(x_final)
+print(x_final.shape)
+print(x_final[0][:1, :2, :2]) #4차원 배열 형태를 다루는 법
+x_final = x_final.flatten()
